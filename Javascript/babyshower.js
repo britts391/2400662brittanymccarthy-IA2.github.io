5a1d7bcd902d033d714f0ef6043f8ff59943e98f
Brittany_McCarthy <brittanytmccarthy@students.utech.edu.jm>
2025-11-20 09:12:59 -0500

    Update babyshower.js

--- Javascript/babyshower.js
+++ Javascript/babyshower.js
@@ -30,13 +30,22 @@
 */
 
 function addToCart(themeName, price, textInputId, errorId) {
-    // Step 1: Get references to the user input and error message area
+// Check if user is logged in before adding to cart
+    // (IA#2 2d - Basic Logic)
+    var username = localStorage.getItem("currentUser");
+    if (!username) {
+        // If no user session is found, show alert and stop
+        alert("User not logged in.");
+        return;
+    }
+
+    // Get references to the user input and error message area
     // Using getElementById() to retrieve specific elements from the DOM
     var babyTextInput = document.getElementById(textInputId);
     var errorMsg = document.getElementById(errorId);
     var babyText = babyTextInput.value.trim(); // trim is use to remove extra spaces
 
-    // Step 2: Input Validation — Ensure backdrop text is entered
+    //  Input Validation — Ensure backdrop text is entered
     // (IA#2 2c - Form Validation)
     if (!babyText) {
         // If the field is empty, show an error message dynamically
@@ -47,16 +56,9 @@ function addToCart(themeName, price, textInputId, errorId) {
         errorMsg.textContent = "";/* error message is now blank*/
     }
 
-    // Step 3: Check if user is logged in before adding to cart
-    // (IA#2 2d - Basic Logic)
-    var username = localStorage.getItem("currentUser");
-    if (!username) {
-        // If no user session is found, show alert and stop
-        alert("User not logged in.");
-        return;
-    }
+    
 
-    // Step 4: Retrieve existing cart data or create a new one
+    //  Retrieve existing cart data or create a new one
     // (IA#2 2a - DOM & Storage Manipulation)
     // Using JSON.parse() to convert stored data back into an object
     var cart = JSON.parse(localStorage.getItem("cart")) || {};
@@ -73,11 +75,11 @@ function addToCart(themeName, price, textInputId, errorId) {
         category: "Baby Shower"// Added category label for identification
     });
 
-    // Step 6: Save updated cart back to localStorage
+    // Save updated cart back to localStorage
     // Convert object to string using JSON.stringify()
     localStorage.setItem("cart", JSON.stringify(cart));
 
-    // Step 7: Provide user feedback (Basic Interactivity)
+    // Provide user feedback (Basic Interactivity)
     // Uses a helper function to calculate updated total and display it
     alert(`Added "${themeName}" (Baby Shower) to cart with backdrop text. Total so far: $${calculateTotal(username)} JMD`);
     /*We use back ticks ` to  create a template literal a special type of string that allows embeded variables and line breaks.
@@ -117,3 +119,4 @@ function calculateTotal(username) {
    • Works with external CSS and HTML for organized, modular design.
    
 */
+
